8e639798fc6534740e75eb6f97c07613db4b9678
ABreus <AB23051990@yandex.ru>
2022-03-13 09:42:45 +0300

    HW5

---
+++ MetricsAgent/Jobs/DotNetMetricJob.cs
@@ -0,0 +1,33 @@
+﻿using MetricsAgent.DAL;
+using Quartz;
+using System.Diagnostics;
+
+namespace MetricsAgent.Jobs
+{
+    public class DotNetMetricJob : IJob
+    {
+        private IDotNetMetricsRepository _repository;
+        // Счётчик для метрики CPU
+        private PerformanceCounter _cpuCounter;
+        public DotNetMetricJob(IDotNetMetricsRepository repository)
+        {
+            _repository = repository;
+            _cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        }
+        public Task Execute(IJobExecutionContext context)
+        {
+            // Получаем значение занятости CPU
+            var cpuUsageInPercents = Convert.ToInt32(_cpuCounter.NextValue());
+            // Узнаем, когда мы сняли значение метрики
+            var time =
+            TimeSpan.FromSeconds(DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+            // Теперь можно записать что-то посредством репозитория
+            _repository.Create(new Models.DotNetMetric
+            {
+                Time = time,
+                Value = cpuUsageInPercents
+            });
+            return Task.CompletedTask;
+        }
+    }
+}
