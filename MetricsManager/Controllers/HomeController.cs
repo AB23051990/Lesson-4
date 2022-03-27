832d16eb4a6b75a0203fe0dfb276d151c75747df
ABreus <AB23051990@yandex.ru>
2022-03-27 13:56:25 +0300

    HW7

---
+++ MetricsManager/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace MetricsManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            // логируем, что мы пошли в соседний сервис
+            Logger.LogInformation($"starting new request to metrics agent");
+            // обращение в сервис
+            var metrics = metricsAgentClient.GetCpuMetrics(new GetAllCpuMetricsRequest
+            {
+                FromTime = fromTime,
+                ToTime = toTime
+            });
+            // возвращаем ответ
+            return Ok(metrics);
+        }
+    }
+}
